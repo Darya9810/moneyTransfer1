37210bc738e63ee91c92e0485a72601d683d6d57
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2021-11-29 23:01:19 +0300

    Main

--- src/maim.java
+++
@@ -1,8 +0,0 @@
-public class maim {
-    public static void main(String[] args) {
-        int balance = 2000000000;
-        int introduction = 500000000;
-        int total = balance + introduction;
-        System.out.println(total);
-    }
-}
